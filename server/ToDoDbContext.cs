6884e15ce6c4257ab775f3f1f2701aabf1eab50b
moriya geva <mog33209@gmail.com>
2025-04-02 19:41:08 +0300

    second

--- server/ToDoDbContext.cs
+++ server/ToDoDbContext.cs
@@ -1,46 +1,103 @@
-﻿// // using System;
-// // using System.Collections.Generic;
+﻿// // // using System;
+// // // using System.Collections.Generic;
+// // // using Microsoft.EntityFrameworkCore;
+// // // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+// // // namespace TodoApi;
+
+// // // public partial class ToDoDbContext : DbContext
+// // // {
+// // //     public ToDoDbContext()
+// // //     {
+// // //     }
+
+// // //     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+// // //         : base(options)
+// // //     {
+// // //     }
+
+// // //     public virtual DbSet<Item> Items { get; set; }
+
+// // //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// // //         => optionsBuilder.UseMySql("ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+
+// // //     protected override void OnModelCreating(ModelBuilder modelBuilder)
+// // //     {
+// // //         modelBuilder
+// // //             .UseCollation("utf8mb4_0900_ai_ci")
+// // //             .HasCharSet("utf8mb4");
+
+// // //         modelBuilder.Entity<Item>(entity =>
+// // //         {
+// // //             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+// // //             entity.ToTable("items");
+
+// // //             entity.Property(e => e.Name).HasMaxLength(100);
+// // //         });
+
+// // //         OnModelCreatingPartial(modelBuilder);
+// // //     }
+
+// // //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// // // }
+// // using System;
 // // using Microsoft.EntityFrameworkCore;
-// // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+// // using Microsoft.Extensions.Configuration;
+// // using System.IO;
 
-// // namespace TodoApi;
-
-// // public partial class ToDoDbContext : DbContext
+// // namespace TodoApi
 // // {
-// //     public ToDoDbContext()
+// //     public partial class ToDoDbContext : DbContext
 // //     {
-// //     }
-
-// //     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-// //         : base(options)
-// //     {
-// //     }
-
-// //     public virtual DbSet<Item> Items { get; set; }
-
-// //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// //         => optionsBuilder.UseMySql("ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
-
-// //     protected override void OnModelCreating(ModelBuilder modelBuilder)
-// //     {
-// //         modelBuilder
-// //             .UseCollation("utf8mb4_0900_ai_ci")
-// //             .HasCharSet("utf8mb4");
-
-// //         modelBuilder.Entity<Item>(entity =>
+// //         public ToDoDbContext()
 // //         {
-// //             entity.HasKey(e => e.Id).HasName("PRIMARY");
+// //         }
 
-// //             entity.ToTable("items");
+// //         public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+// //             : base(options)
+// //         {
+// //         }
 
-// //             entity.Property(e => e.Name).HasMaxLength(100);
-// //         });
+// //         public virtual DbSet<Item> Items { get; set; }
 
-// //         OnModelCreatingPartial(modelBuilder);
+// //         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// //         {
+// //             if (!optionsBuilder.IsConfigured)
+// //             {
+// //                 var configuration = new ConfigurationBuilder()
+// //                     .SetBasePath(Directory.GetCurrentDirectory())
+// //                     .AddJsonFile("appsettings.json")
+// //                     .Build();
+
+// //                 var connectionString = configuration.GetConnectionString("ToDoDB");
+
+// //                 optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+// //             }
+// //         }
+
+// //         protected override void OnModelCreating(ModelBuilder modelBuilder)
+// //         {
+// //             modelBuilder
+// //                 .UseCollation("utf8mb4_0900_ai_ci")
+// //                 .HasCharSet("utf8mb4");
+
+// //             modelBuilder.Entity<Item>(entity =>
+// //             {
+// //                 entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+// //                 entity.ToTable("items");
+
+// //                 entity.Property(e => e.Name).HasMaxLength(100);
+// //             });
+
+// //             OnModelCreatingPartial(modelBuilder);
+// //         }
+
+// //         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 // //     }
-
-// //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 // // }
+
 // using System;
 // using Microsoft.EntityFrameworkCore;
 // using Microsoft.Extensions.Configuration;
@@ -50,13 +107,12 @@
 // {
 //     public partial class ToDoDbContext : DbContext
 //     {
-//         public ToDoDbContext()
-//         {
-//         }
+//         private readonly IConfiguration _configuration;
 
-//         public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+//         public ToDoDbContext(DbContextOptions<ToDoDbContext> options, IConfiguration configuration)
 //             : base(options)
 //         {
+//             _configuration = configuration;
 //         }
 
 //         public virtual DbSet<Item> Items { get; set; }
@@ -65,13 +121,7 @@
 //         {
 //             if (!optionsBuilder.IsConfigured)
 //             {
-//                 var configuration = new ConfigurationBuilder()
-//                     .SetBasePath(Directory.GetCurrentDirectory())
-//                     .AddJsonFile("appsettings.json")
-//                     .Build();
-
-//                 var connectionString = configuration.GetConnectionString("ToDoDB");
-
+//                 var connectionString = _configuration.GetConnectionString("ToDoDB");
 //                 optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 //             }
 //         }
@@ -97,7 +147,6 @@
 //         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 //     }
 // }
-
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -139,6 +188,11 @@ namespace TodoApi
                 entity.ToTable("items");
 
                 entity.Property(e => e.Name).HasMaxLength(100);
+                
+                // הוספת העמודה IsComplete
+                entity.Property(e => e.IsComplete)
+                      .HasColumnType("BOOLEAN")
+                      .HasDefaultValue(false);
             });
 
             OnModelCreatingPartial(modelBuilder);
@@ -146,4 +200,5 @@ namespace TodoApi
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
+    
 }
