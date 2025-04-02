610437bd5ae55ca40cba7ac2a6bb9166826fdb90
moriya geva <mog33209@gmail.com>
2025-04-02 19:23:49 +0300

    update

--- server/ToDoDbContext.cs
+++ server/ToDoDbContext.cs
@@ -1,43 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// // using System;
+// // using System.Collections.Generic;
+// // using Microsoft.EntityFrameworkCore;
+// // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+// // namespace TodoApi;
+
+// // public partial class ToDoDbContext : DbContext
+// // {
+// //     public ToDoDbContext()
+// //     {
+// //     }
+
+// //     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+// //         : base(options)
+// //     {
+// //     }
+
+// //     public virtual DbSet<Item> Items { get; set; }
+
+// //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// //         => optionsBuilder.UseMySql("ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+
+// //     protected override void OnModelCreating(ModelBuilder modelBuilder)
+// //     {
+// //         modelBuilder
+// //             .UseCollation("utf8mb4_0900_ai_ci")
+// //             .HasCharSet("utf8mb4");
+
+// //         modelBuilder.Entity<Item>(entity =>
+// //         {
+// //             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+// //             entity.ToTable("items");
+
+// //             entity.Property(e => e.Name).HasMaxLength(100);
+// //         });
+
+// //         OnModelCreatingPartial(modelBuilder);
+// //     }
+
+// //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// // }
+// using System;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Configuration;
+// using System.IO;
+
+// namespace TodoApi
+// {
+//     public partial class ToDoDbContext : DbContext
+//     {
+//         public ToDoDbContext()
+//         {
+//         }
+
+//         public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+//             : base(options)
+//         {
+//         }
+
+//         public virtual DbSet<Item> Items { get; set; }
+
+//         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//         {
+//             if (!optionsBuilder.IsConfigured)
+//             {
+//                 var configuration = new ConfigurationBuilder()
+//                     .SetBasePath(Directory.GetCurrentDirectory())
+//                     .AddJsonFile("appsettings.json")
+//                     .Build();
+
+//                 var connectionString = configuration.GetConnectionString("ToDoDB");
+
+//                 optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+//             }
+//         }
+
+//         protected override void OnModelCreating(ModelBuilder modelBuilder)
+//         {
+//             modelBuilder
+//                 .UseCollation("utf8mb4_0900_ai_ci")
+//                 .HasCharSet("utf8mb4");
+
+//             modelBuilder.Entity<Item>(entity =>
+//             {
+//                 entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//                 entity.ToTable("items");
+
+//                 entity.Property(e => e.Name).HasMaxLength(100);
+//             });
+
+//             OnModelCreatingPartial(modelBuilder);
+//         }
+
+//         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+//     }
+// }
+
+using System;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
-namespace TodoApi;
-
-public partial class ToDoDbContext : DbContext
+namespace TodoApi
 {
-    public ToDoDbContext()
+    public partial class ToDoDbContext : DbContext
     {
-    }
+        private readonly IConfiguration _configuration;
 
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Item> Items { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .UseCollation("utf8mb4_0900_ai_ci")
-            .HasCharSet("utf8mb4");
-
-        modelBuilder.Entity<Item>(entity =>
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options, IConfiguration configuration)
+            : base(options)
         {
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
+            _configuration = configuration;
+        }
 
-            entity.ToTable("items");
+        public virtual DbSet<Item> Items { get; set; }
 
-            entity.Property(e => e.Name).HasMaxLength(100);
-        });
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("ToDoDB");
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+        }
 
-        OnModelCreatingPartial(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+                entity.ToTable("items");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
